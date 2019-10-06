653ecc92f411b5b084d613e496232e1876195bee
Zuz_Pacut <pacutovazuzana@gmail.com>
2019-10-06 15:02:44 +0200

    Save to Music

--- application.sh
+++ application.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-CLIENT_ID=""
+MUSIC_DIR="$HOME/Music"
 
 _to_api_url() {
     local -r sc_url="$1" # For example, https://soundcloud.com/mayanwarriorofficial/sainte-vie-bm-2019
-    echo "$(curl -s "https://api.soundcloud.com/resolve?url=$sc_url&client_id=$CLIENT_ID" | jq -r '.location')"
+    echo "$(curl -s "https://api.soundcloud.com/resolve?url=$sc_url&client_id=$SC_CLIENT_ID" | jq -r '.location')"
 }
 
+# download_track SC_URL [DEST_DIR]
 download_track() {
     local -r sc_url="$1"
+    local -r dest_dir="${2:-"$MUSIC_DIR"}"
     local -r api_url="$(_to_api_url "$sc_url")" # client_id is already there
-    
     local -r track_info="$(curl -s "$api_url")"
-    local -r title="$(echo "$track_info" | jq -r '.title')"
-    local -r stream_url="$(echo "$track_info" | jq -r '.stream_url')?client_id=$CLIENT_ID"
-    
+    local -r stream_url="$(echo "$track_info" | jq -r '.stream_url')?client_id=$SC_CLIENT_ID"
     local -r cdn_url="$(curl -s "$stream_url" | jq -r ".location")"
-    local -r file_name="$title.mp3"
+    local -r file_name="$(echo "$track_info" | jq -r '.title').mp3"
+    local -r file_path="$dest_dir/$file_name"
 
-    curl -# "$cdn_url" -o "$file_name"
+    curl -# "$cdn_url" -o "$file_path"
 }
 
 $@
